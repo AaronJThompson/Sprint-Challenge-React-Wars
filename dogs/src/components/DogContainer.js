18da5ae38c0fc13a44c69fc60be5b97f132ba6dd
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-31 10:54:15 +0100

    Stretch: Create dog components

---
+++ dogs/src/components/DogContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Dog from './Dog';
+export default function DogContainer(props) {
+    let { dogs } = props;
+
+    function createDogs(dogs){
+        let dogComponenets = []
+        for (const breed in dogs) {
+            let componenet = (
+                <Dog
+                    breed={breed}
+                    image={dogs[breed].imageURL}
+                />
+            )
+            dogComponenets.push(componenet);
+        }
+        return dogComponenets;
+    }
+    return (
+        <div className="dog-container">
+            {
+                createDogs(dogs);
+            }
+        </div>
+    )
+}
