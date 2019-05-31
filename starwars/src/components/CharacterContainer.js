7c4ab94f2f84a012a31caccb7f829c17f8ad6a6a
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-31 08:49:24 +0100

    Add regex to get id for key prop

--- starwars/src/components/CharacterContainer.js
+++ starwars/src/components/CharacterContainer.js
@@ -3,12 +3,19 @@ import './StarWars.css';
 import Character from './Character';
 export default function CharacterContainer(props){
     let { charData } = props;
-
+    function addIdsToCharacter(chars) {
+        let matchPeopleId = /(?<=people\/)\d+/
+        return chars.map((char) => {
+            char.id = char.url.match(matchPeopleId)[0];
+            return char;
+        })
+    }
     return (
         <div className="character-container">
             {
                 charData.map((char) => {
                     return (
+                        <Character
                     )
                 })
             }
